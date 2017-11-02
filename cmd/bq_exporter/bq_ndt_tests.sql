d954aac2559ab3b21de75b5b4f29bccd262a8691
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-11-01 22:13:04 -0400

    Use the refresh rate to bin test counts.

--- cmd/bq_exporter/bq_ndt_tests.sql
+++ cmd/bq_exporter/bq_ndt_tests.sql
@@ -1,13 +1,4 @@
 SELECT
---  CASE
---    WHEN DAYOFWEEK(log_time) == 1 THEN "Sunday"
---    WHEN DAYOFWEEK(log_time) == 2 THEN "Monday"
---    WHEN DAYOFWEEK(log_time) == 3 THEN "Tuesday"
---    WHEN DAYOFWEEK(log_time) == 4 THEN "Wednesday"
---    WHEN DAYOFWEEK(log_time) == 5 THEN "Thursday"
---    WHEN DAYOFWEEK(log_time) == 6 THEN "Friday"
---    WHEN DAYOFWEEK(log_time) == 7 THEN "Saturday"
---    ELSE "Bananas" END as day_of_week,
   CASE 
     WHEN connection_spec.data_direction == 0 THEN "c2s"
     WHEN connection_spec.data_direction == 1 THEN "s2c"
@@ -28,8 +19,9 @@ FROM
     [measurement-lab:public.ndt]
 
 WHERE
-    TIMESTAMP_TO_USEC(log_time) > INTEGER(TIMESTAMP_TO_USEC(CURRENT_TIMESTAMP()) / (15 * 60 * 1000000)) * (15 * 60 * 1000000) - (24 * 60 * 60 * 1000000)
-AND TIMESTAMP_TO_USEC(log_time) < INTEGER(TIMESTAMP_TO_USEC(CURRENT_TIMESTAMP()) / (15 * 60 * 1000000)) * (15 * 60 * 1000000) - (24 * 60 * 60 * 1000000) + (15 * 60 * 1000000)
+    -- Count all tests in a REFRESH_RATE_SEC interval 36 hours ago.
+    TIMESTAMP_TO_SEC(log_time) > INTEGER(TIMESTAMP_TO_SEC(CURRENT_TIMESTAMP()) / (REFRESH_RATE_SEC)) * (REFRESH_RATE_SEC) - (36 * 60 * 60)
+AND TIMESTAMP_TO_SEC(log_time) < INTEGER(TIMESTAMP_TO_SEC(CURRENT_TIMESTAMP()) / (REFRESH_RATE_SEC)) * (REFRESH_RATE_SEC) - (36 * 60 * 60) + (REFRESH_RATE_SEC)
 
 GROUP BY label_machine, label_direction
 ORDER BY value
